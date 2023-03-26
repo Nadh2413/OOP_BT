a9411b86d31bab5211e6098f33c78680e4877be4
cdhuan <cdhuan1003@gmail.com>
2023-03-26 18:18:36 +0700

    Khai bao - khoi tao

--- src/data/Dog.java
+++ src/data/Dog.java
@@ -2,39 +2,48 @@
 package data;
 
 public class Dog {
-//	class gom tất cả các cái Object có chung đặc điểm
-//	Chứa đặc điểm , chi tiết  cái khuôn 
+//	tạo khung ( khai báo khung )
 	private String name;
 	private int yob;
 	private double weight;
 	
-//	Có khuôn thì cần có phễu để đổ vật liệu vào
-//	nhận vật liệu, construct object
-//	Phễu là hàm không có giá trị trả về, không void luôn 
-//	Mỗi lần call là một lần create vì là tạo ra ( hàm khởi tạo)
-//	Mỗi lần gọi là sinh ra một Object mới
-	public Dog(String iName, int iYob, double iWeight) {
-		name = iName;
-		yob = iYob;
-		weight = iWeight;
+//	Khởi tạo các  một object ( nó như là cái phễu )
+	public Dog(String name, int yob, double weight) {
+		this.name = name;
+		this.yob = yob;
+		this.weight = weight;
 	}
-//	Object là một hành động, behaviour , xử lý tính toán xử  lý trên biến từ lúc đúc ra
-//	Hàm gọi ( gọi nhìu lần ) các hành động lặp đi lặp lại  trên Object đó
-	public void bark() {
-//		System.out.println("My name is " + name);
-//		System.out.println("My yob is " + yob);
-//		%-10s - in ra chuỗi chiếm 10 space căn trái , 4d,4f là chiếm 4 kí tự 
-		System.out.printf("Name: %-10s - Yob: %4d - Weight: %4.1f\n",name,yob,weight);
+
+	public String getName() {
+		return name;
 	}
-//	Thay đổi info ban đầu 
-	public void setNewWeight(double newWeight) {
-		weight = newWeight;
-		
+
+	public void setName(String name) {
+		this.name = name;
 	}
-//	Mặc định là Info sẽ được giấu , khi giao tiếp mới bật ra
-//	Chia sẻ Info khi được hỏi , đó gouj là method hàm hành động get()
-	
+
 	public int getYob() {
 		return yob;
 	}
+
+	public void setYob(int yob) {
+		this.yob = yob;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public void bark() {
+		System.out.println("go go ...");
+		System.out.println("Bark name : "+ name);
+	}
+	
+	
+	
+	
 }
--- src/data/Main.java
+++ src/data/Main.java
@@ -1,21 +1,24 @@
 package data;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		new xin vùng nhớ mới 
 		Dog nic = new Dog("NicSon", 2023,0.75);
-		nic.bark();
-//		ENCAPSULATION : Tính đóng gói thông tin và hành động , gói mọi thứ vào trong class
-//		sẽ nhân bản ra Object , Object nào sẽ hành động trong khuôn khổ Object đó
-//		SetWeight
-		System.out.println("After Weight ");
-		nic.setNewWeight(5);
+		Dog ky = new Dog("Kyy",2022,0.98);
+		
+		System.out.println("Name : " + nic.getName());
 		nic.bark();
 		
-		int nicYob = nic.getYob();
-		System.out.println("Yob : " + nicYob);
+//		ENCAPSULATION : Tính đóng gói thông tin và hành động , gói mọi thứ vào trong class
+//		sẽ nhân bản ra Object , Object nào sẽ hành động trong khuôn khổ Object đó
+//		SetWeight 
+		System.out.println("Weight before changer: " + nic.getWeight());
+		nic.setWeight(5);
+		System.out.println("Weight after changer: " + nic.getWeight());
+		System.out.println("Yob : " + nic.getYob());
+		
+//		
 	}
 
 }
